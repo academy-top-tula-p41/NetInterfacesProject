a425f61925dd6cd551d73fa64926e1397b10192b
Maxim Efimov <efimov.top.academy@gmail.com>
2026-05-18 21:04:08 +0300

    Добавьте файлы проекта.

---
+++ NetInterfacesProject/Examples.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetInterfacesProject
+{
+    static class Examples
+    {
+        public static void InterfacesWelcomeExample()
+        {
+            /*
+            Methods
+            Properties
+            Indexators []
+            Events
+            Static field, const field
+            */
+
+            MovesObject obj = new MovesObject();
+            (obj as IMovable).DefaultMethod();
+
+            IMovable movable = new MovesObject();
+            movable.DefaultMethod();
+        }
+    }
+
+    class MovesObject : IMovable
+    {
+        int speed;
+        public int Speed { get; set; }
+        //{
+        //    get => speed;
+        //    set => speed = value; 
+        //}
+
+        public event Action<string> MoveHandler;
+
+        public void Move()
+        {
+            Console.WriteLine("Movable Object Move");
+            MoveHandler?.Invoke("Movable Object Move Handler");
+        }
+    }
+    interface IMovable
+    {
+        const int minSpeed = 0;
+        static int maxSpeed = 100;
+        int Speed { get; set; }
+        void Move();
+
+        void DefaultMethod()
+        {
+            Console.WriteLine("Interface method");
+        }
+
+        event Action<string> MoveHandler;
+    }
+}
---
+++ NetInterfacesProject/Program.cs
@@ -0,0 +1,52 @@
+﻿
+DoFly(new Duck());
+DoFly(new Planet());
+DoFly(new Airplane());
+
+IFlyable[] flyables = new IFlyable[4];
+flyables[0] = new Duck();
+flyables[1] = new Planet();
+flyables[2] = new Airplane();
+flyables[3] = new Disk();
+
+foreach (var flyable in flyables)
+    flyable.Fly();
+
+void DoFly(IFlyable flable)
+{
+    flable.Fly();
+}
+
+interface IFlyable
+{
+    void Fly() => Console.WriteLine("Any flying");
+}
+
+class Airplane : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("Arplane flying");
+    }
+}
+
+class Duck : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("Duck flying");
+    }
+}
+
+class Planet : IFlyable
+{
+    public void Fly()
+    {
+        Console.WriteLine("Planet flying");
+    }
+}
+
+class Disk : IFlyable
+{
+
+}
